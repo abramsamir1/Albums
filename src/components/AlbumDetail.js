8a83115bfb354f6b5825fb8a8f7d323df398fccd
Abram Samir <abramsamir1@gmail.com>
2018-02-19 14:14:25 +0200

    Albums project

---
+++ src/components/AlbumDetail.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import { Text, View, Image, Linking } from 'react-native';
+import Card from './Card';
+import CardSection from './CardSection';
+import Button from './Button';
+
+const AlbumDetail = ({ albumData }) => {
+  const { title, artist, thumbnail_image, image, url } = albumData;
+  const { thumbnailStyle,
+          headerContentStyle,
+          thumbnailContainerStyle,
+          headerTextStyle,
+          imageStyle } = styles;
+return (
+  //elcard hayetbe3etlo kol ely ta7to da 3ashan el card feh aktar mn section
+  <Card>
+    <CardSection>
+      <View style={thumbnailContainerStyle}>
+        <Image
+          style={thumbnailStyle}
+         source={{ uri: thumbnail_image }} />
+      </View>
+      <View style={headerContentStyle}>
+        <Text style={headerTextStyle}>{title}</Text>
+        <Text>{artist}</Text>
+      </View>
+    </CardSection>
+
+    <CardSection>
+      <Image
+      style={imageStyle}
+      source={{ uri: image }} />
+    </CardSection>
+
+    <CardSection>
+    <Button onPressed={() => Linking.openURL(url)} >
+      Buy Now
+    </Button>
+    </CardSection>
+  </Card>
+  );
+};
+
+const styles = {
+  headerContentStyle: {
+    flexDirection: 'column',
+    justifyContent: 'space-around'
+  },
+  headerTextStyle: {
+    fontSize: 18
+  },
+  thumbnailStyle: {
+  height: 50,
+  width: 50
+},
+  thumbnailContainerStyle: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: 10,
+    marginRight: 10
+  },
+  imageStyle: {
+    height: 300,
+    flex: 1,
+    width: null
+
+  }
+
+};
+
+export default AlbumDetail;
